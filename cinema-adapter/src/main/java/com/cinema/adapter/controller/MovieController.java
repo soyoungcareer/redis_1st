570d5523736d7bcf08f5a702b83e14a35cf6bd40
soyoungcareer <soyoungcareer@gmail.com>
2025-01-13 22:52:10 +0900

    modify : 생성자 lombok Annotation 으로 변경

--- cinema-adapter/src/main/java/com/cinema/adapter/controller/MovieController.java
+++ cinema-adapter/src/main/java/com/cinema/adapter/controller/MovieController.java
@@ -2,23 +2,23 @@ package com.cinema.adapter.controller;
 
 import com.cinema.application.service.MovieService;
 import com.cinema.core.domain.Movie;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
     private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
-
+    // TODO : 예약 가능 여부는 query parameter로 구분: /api/movies?bookable=true
     @GetMapping("/bookable")
     public List<Movie> getBookableMovies() {
         return movieService.getBookableMovies();
     }
 }
+
--- cinema-application/src/main/java/com/cinema/application/service/MovieService.java
+++ cinema-application/src/main/java/com/cinema/application/service/MovieService.java
@@ -2,17 +2,15 @@ package com.cinema.application.service;
 
 import com.cinema.core.domain.Movie;
 import com.cinema.infra.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
-
-    public MovieService(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
 
     public List<Movie> getBookableMovies() {
         return movieRepository.findBookableMovies();
